a069ca206bdd9fcd581ab5306fd16cf6259e3905
maury07 <maury.-n@live.com.ar>
2021-07-10 19:03:49 -0300

    Version 1

---
+++ PruebaSistemaTurnos/frmHome.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaSistemaTurnos
+{
+    public partial class frmHome : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
